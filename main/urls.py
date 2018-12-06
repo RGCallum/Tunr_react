11e57187a97b6e39279de26d46236ac959f85110
Rguy <rguy1290@gmail.com>
2018-12-06 15:16:09 -0500

    did some stuff to fix heroku

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from main import views
+from . import views
 
 router = routers.DefaultRouter()
 router.register('artists', views.ArtistView)
