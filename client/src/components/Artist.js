d5df7a94d8b5e5d570c7dfd1358de05e6cbe3669
Rguy <rguy1290@gmail.com>
2018-12-06 16:50:13 -0500

    still trying for heroku

--- client/src/components/Artist.js
+++ client/src/components/Artist.js
@@ -1,22 +1,43 @@
 import React, {Component} from 'react';
-import axios from 'axios'
-
+import axios from 'axios';
 
 class Artist extends Component {
-      state = {
+
+    state = {
             artist: {},
             songs: [],
     }
 
     componentDidMount() {
-        const id = this.props.match.params.id
-
-        axios.get(``)
+        const artistId = this.props.match.params.id;
+        this.fetchArtist(artistId)
     }
+
+    fetchArtist = async (artistId) => {
+        try {
+            const artistResponse = await axios.get(`/api/artists/${artistId}`)
+            await this.setState({
+                artist: artistResponse.data,
+                songs: artistResponse.data.songs,
+            })
+        }
+        catch (error) {
+            console.log(error)
+            await this.setState({error: error.message})
+        }
+    }
+
     render() {
         return (
             <div>
-               hola Artist
+                <img src={this.state.artist.photo_url} alt=""/>
+                <h1>{this.state.artist.name}</h1>
+                {this.state.songs.map(song => (
+                    <div key={song.id}>
+                        <h4>{song.title}</h4>
+                        <audio controls src={song.preview_url}></audio>
+                    </div>
+                ))}
             </div>
         );
     }
